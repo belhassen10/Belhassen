8bb03f39af26b7f99b223bb6d272ecc6c46cef9a
belhassen10 <belhassen.amdoun@etudiant-enit.utm.tn>
2024-09-05 10:29:46 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+# Demander deux nombres à l'utilisateur
+nombre1 = 2
+nombre2 = 10
+
+# Calculer la somme
+somme = nombre1 + nombre2
+
+# Afficher le résultat
+print(f"La somme de {nombre1} et {nombre2} est {somme}.")
